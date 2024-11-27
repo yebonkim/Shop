f54cd642f221cc6c081d208869c9b76f37c793c3
Yebon Kim <v954242@gmail.com>
2024-11-27 23:17:49 +0900

    feat : add loading showcase logic

--- app/src/main/java/com/example/shop/feature/home/HomeViewModel.kt
+++ app/src/main/java/com/example/shop/feature/home/HomeViewModel.kt
@@ -1,30 +1,47 @@
 package com.example.shop.feature.home
 
+import com.airbnb.mvrx.Async
 import com.airbnb.mvrx.MavericksState
 import com.airbnb.mvrx.MavericksViewModel
 import com.airbnb.mvrx.MavericksViewModelFactory
+import com.airbnb.mvrx.Uninitialized
 import com.airbnb.mvrx.hilt.AssistedViewModelFactory
 import com.airbnb.mvrx.hilt.hiltMavericksViewModelFactory
-import com.example.shop.domain.model.PaginationData
+import com.example.shop.domain.ShowcaseRepository
 import com.example.shop.domain.model.Showcase
 import dagger.assisted.Assisted
 import dagger.assisted.AssistedFactory
 import dagger.assisted.AssistedInject
+import kotlinx.coroutines.flow.first
 
 data class HomeUiState(
-  val isLoading: Boolean = false,
-  val showcases: List<Showcase> = emptyList(),
-  private val pageMap: Map<String, PaginationData> = emptyMap()
-): MavericksState
+  val showcases: Async<List<Showcase>> = Uninitialized,
+) : MavericksState
 
 class HomeViewModel @AssistedInject constructor(
   @Assisted initialState: HomeUiState,
+  private val showcaseRepository: ShowcaseRepository,
 ) : MavericksViewModel<HomeUiState>(initialState) {
+
+  init {
+    loadData()
+  }
+
+  private fun loadData() {
+    suspend {
+      showcaseRepository.load().first()
+    }.execute { showcases ->
+      copy(
+        showcases = showcases,
+      )
+    }
+  }
 
   @AssistedFactory
   interface Factory : AssistedViewModelFactory<HomeViewModel, HomeUiState> {
     override fun create(state: HomeUiState): HomeViewModel
   }
 
-  companion object : MavericksViewModelFactory<HomeViewModel, HomeUiState> by hiltMavericksViewModelFactory()
+  companion object :
+    MavericksViewModelFactory<HomeViewModel, HomeUiState> by hiltMavericksViewModelFactory()
 }
