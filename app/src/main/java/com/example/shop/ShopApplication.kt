e7deab27a9eb2cda17add55110262774fd6c5176
Yebon Kim <v954242@gmail.com>
2024-11-27 22:56:43 +0900

    feat : init mavericks

--- app/src/main/java/com/example/shop/ShopApplication.kt
+++ app/src/main/java/com/example/shop/ShopApplication.kt
@@ -1,9 +1,13 @@
 package com.example.shop
 
 import android.app.Application
+import com.airbnb.mvrx.Mavericks
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
 class ShopApplication: Application() {
-
+  override fun onCreate() {
+    super.onCreate()
+    Mavericks.initialize(this)
+  }
 }
