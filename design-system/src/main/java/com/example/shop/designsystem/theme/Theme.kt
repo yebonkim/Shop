e48b7d305d1e98d38a6ac2425449d220e777b06f
Yebon Kim <v954242@gmail.com>
2024-11-27 21:31:31 +0900

    update : material files

--- design-system/src/main/java/com/example/shop/designsystem/theme/Theme.kt
+++ design-system/src/main/java/com/example/shop/designsystem/theme/Theme.kt
@@ -9,18 +9,27 @@ import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 
 private val DarkColorScheme = darkColorScheme(
-  primary = Purple80,
+  primary = Color.White,
+  onPrimary = Color.Black,
   secondary = PurpleGrey80,
-  tertiary = Pink80
+  tertiary = Pink80,
+  surface = Color.Black,
+  background = Grey,
+  scrim = Red
 )
 
 private val LightColorScheme = lightColorScheme(
-  primary = Purple40,
+  primary = Color.Black,
+  onPrimary = Color.White,
   secondary = PurpleGrey40,
-  tertiary = Pink40
+  tertiary = Pink40,
+  surface = Color.White,
+  background = Grey,
+  scrim = Red
 
   /* Other default colors to override
     background = Color(0xFFFFFBFE),
@@ -37,7 +46,7 @@ private val LightColorScheme = lightColorScheme(
 fun ShopTheme(
   darkTheme: Boolean = isSystemInDarkTheme(),
   // Dynamic color is available on Android 12+
-  dynamicColor: Boolean = true,
+  dynamicColor: Boolean = false,
   content: @Composable () -> Unit
 ) {
   val colorScheme = when {
--- design-system/src/main/java/com/example/shop/designsystem/theme/Type.kt
+++ design-system/src/main/java/com/example/shop/designsystem/theme/Type.kt
@@ -1,6 +1,7 @@
 package com.example.shop.designsystem.theme
 
 import androidx.compose.material3.Typography
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
@@ -14,21 +15,19 @@ val Typography = Typography(
     fontSize = 16.sp,
     lineHeight = 24.sp,
     letterSpacing = 0.5.sp
+  ),
+  titleLarge = TextStyle(
+    fontFamily = FontFamily.Default,
+    fontWeight = FontWeight.Medium,
+    fontSize = 20.sp,
+    lineHeight = 28.sp,
+    letterSpacing = 0.sp
+  ),
+  labelMedium = TextStyle(
+    fontFamily = FontFamily.Default,
+    fontWeight = FontWeight.Medium,
+    fontSize = 12.sp,
+    lineHeight = 16.sp,
+    letterSpacing = 0.5.sp
   )
-  /* Other default text styles to override
-    titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 22.sp,
-        lineHeight = 28.sp,
-        letterSpacing = 0.sp
-    ),
-    labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Medium,
-        fontSize = 11.sp,
-        lineHeight = 16.sp,
-        letterSpacing = 0.5.sp
-    )
-    */
 )
