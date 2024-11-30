0074e57ef25570b91146a92b277f4c6ee8981f0a
Yebon Kim <v954242@gmail.com>
2024-11-30 16:58:42 +0900

    feat : add copy func of Contents

--- domain/src/main/java/com/example/shop/domain/model/Contents.kt
+++ domain/src/main/java/com/example/shop/domain/model/Contents.kt
@@ -25,3 +25,33 @@ sealed interface Contents {
     override val items: List<ContentsItemType>
   ) : Contents
 }
+
+fun Contents.copy(items: List<ContentsItemType>): Contents {
+  return when (this) {
+    is Contents.BannerContents -> {
+      validateItems<ContentsItemType.Banner>(items)
+      copy(items = items as List<ContentsItemType.Banner>)
+    }
+    is Contents.GridContents -> {
+      validateItems<ContentsItemType.Goods>(items)
+      copy(items = items as List<ContentsItemType.Goods>)
+    }
+    is Contents.ScrollContents -> {
+      validateItems<ContentsItemType.Goods>(items)
+      copy(items = items as List<ContentsItemType.Goods>)
+    }
+    is Contents.StyleContents -> {
+      validateItems<ContentsItemType.Style>(items)
+      copy(items = items as List<ContentsItemType.Style>)
+    }
+    is Contents.Unknown -> {
+      copy(items = items)
+    }
+  }
+}
+
+private inline fun <reified T : ContentsItemType> validateItems(items: List<ContentsItemType>) {
+  if (items.any { it !is T }) {
+    throw IllegalArgumentException("Expected ContentsType.${T::class.java.simpleName}, but found: ${items.filter { it !is T }}")
+  }
+}
