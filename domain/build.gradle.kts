d189b8e30dfbf20a9a22fb4dfde2a535580d0f82
Yebon Kim <v954242@gmail.com>
2024-11-30 18:12:57 +0900

    chore : enable kotest

--- domain/build.gradle.kts
+++ domain/build.gradle.kts
@@ -8,6 +8,10 @@ java {
   targetCompatibility = JavaVersion.VERSION_11
 }
 
+tasks.test {
+  useJUnitPlatform()
+}
+
 kotlin {
   compilerOptions {
     jvmTarget = org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_11
@@ -16,4 +20,8 @@ kotlin {
 
 dependencies {
   implementation(libs.kotlinx.coroutines)
+
+  testImplementation(libs.kotest.runner.junit5)
+  testImplementation(libs.kotest.assersions.core)
+  testImplementation(libs.kotest.property)
 }
