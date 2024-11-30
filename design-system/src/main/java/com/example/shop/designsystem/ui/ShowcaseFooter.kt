04fcc88655425fda48b3e5e0b1e120d5508d08bd
Yebon Kim <v954242@gmail.com>
2024-11-30 17:06:13 +0900

    feat : add header, footer ui

---
+++ design-system/src/main/java/com/example/shop/designsystem/ui/ShowcaseFooter.kt
@@ -0,0 +1,81 @@
+package com.example.shop.designsystem.ui
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.bumptech.glide.integration.compose.ExperimentalGlideComposeApi
+import com.bumptech.glide.integration.compose.GlideImage
+import com.example.shop.designsystem.theme.ShopTheme
+import com.example.shop.designsystem.ui.component.AsyncImage
+import com.example.shop.domain.model.Footer
+import com.example.shop.domain.model.FooterType
+
+@Composable
+fun ShowcaseFooter(
+  modifier: Modifier = Modifier,
+  footer: Footer,
+  onClick: () -> Unit,
+) {
+  Row(
+    modifier = modifier
+      .fillMaxWidth()
+      .padding(horizontal = 12.dp, vertical = 20.dp)
+      .background(
+        color = MaterialTheme.colorScheme.primary,
+        shape = RoundedCornerShape(12.dp)
+      )
+      .clickable { onClick() }
+      .padding(12.dp),
+    horizontalArrangement = Arrangement.Center,
+    verticalAlignment = Alignment.CenterVertically,
+  ) {
+    footer.iconUrl?.let { iconUrl ->
+      AsyncImage(
+        model = iconUrl,
+        contentDescription = "Footer icon",
+        modifier = Modifier.size(20.dp),
+      )
+    }
+    Text(
+      text = footer.title,
+      modifier = Modifier
+        .weight(1f, fill = false)
+        .padding(start = 4.dp),
+      color = MaterialTheme.colorScheme.onPrimary,
+      textAlign = TextAlign.Center,
+      overflow = TextOverflow.Ellipsis,
+      maxLines = 1,
+      style = MaterialTheme.typography.bodyLarge,
+    )
+  }
+}
+
+@Composable
+@Preview
+fun ShowcaseFooterPreview() {
+  ShopTheme {
+    ShowcaseFooter(
+      footer = Footer(
+        type = FooterType.MORE,
+        title = "Header TitleHeader TitleHeader TitleHeader Title",
+        iconUrl = "",
+      ),
+      onClick = {}
+    )
+  }
+}
---
+++ design-system/src/main/java/com/example/shop/designsystem/ui/ShowcaseHeader.kt
@@ -0,0 +1,87 @@
+package com.example.shop.designsystem.ui
+
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextDecoration
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.bumptech.glide.integration.compose.ExperimentalGlideComposeApi
+import com.bumptech.glide.integration.compose.GlideImage
+import com.example.shop.designsystem.R
+import com.example.shop.designsystem.theme.ShopTheme
+import com.example.shop.designsystem.ui.component.AsyncImage
+import com.example.shop.domain.model.Header
+
+@Composable
+fun ShowcaseHeader(
+  modifier: Modifier = Modifier,
+  header: Header,
+  onClickLink: ((String) -> Unit)? = null,
+) {
+  Row(
+    modifier = modifier
+      .fillMaxWidth()
+      .padding(horizontal = 12.dp, vertical = 20.dp),
+    horizontalArrangement = Arrangement.SpaceBetween,
+  ) {
+    Row(
+      modifier = Modifier
+        .weight(1f, fill = false)
+        .padding(end = 16.dp),
+      verticalAlignment = Alignment.CenterVertically,
+    ) {
+      Text(
+        text = header.title,
+        modifier = Modifier
+          .weight(1f, fill = false)
+          .padding(end = 8.dp),
+        color = MaterialTheme.colorScheme.onSurface,
+        maxLines = 2,
+        style = MaterialTheme.typography.titleLarge,
+      )
+      header.iconUrl?.let { iconUrl ->
+        AsyncImage(
+          model = iconUrl,
+          contentDescription = "Header icon",
+          modifier = Modifier.size(20.dp),
+        )
+      }
+    }
+    header.linkUrl?.let { linkUrl ->
+      Text(
+        text = stringResource(R.string.more),
+        modifier = Modifier
+          .align(Alignment.Bottom)
+          .clickable { onClickLink?.invoke(linkUrl) },
+        color = MaterialTheme.colorScheme.onSurfaceVariant,
+        maxLines = 1,
+        textDecoration = TextDecoration.Underline,
+        style = MaterialTheme.typography.labelMedium,
+      )
+    }
+  }
+}
+
+@Composable
+@Preview
+fun ShowcaseHeaderPreview() {
+  ShopTheme {
+    ShowcaseHeader(
+      header = Header(
+        title = "Header TitleHeader TitleHeader TitleHeader Title",
+        iconUrl = "https://image.msscdn.net/icons/mobile/clock.png",
+        linkUrl = "https://www.example.com"
+      )
+    )
+  }
+}
