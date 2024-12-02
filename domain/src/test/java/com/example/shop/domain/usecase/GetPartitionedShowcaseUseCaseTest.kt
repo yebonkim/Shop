89ca9bb6a099c6f12ebfef29a44387bc0f2654db
Yebon Kim <v954242@gmail.com>
2024-12-02 11:09:47 +0900

    fix : partitionUseCase

--- domain/src/test/java/com/example/shop/domain/usecase/GetPartitionedShowcaseUseCaseTest.kt
+++ domain/src/test/java/com/example/shop/domain/usecase/GetPartitionedShowcaseUseCaseTest.kt
@@ -15,7 +15,7 @@ import io.mockk.mockk
 class GetPartitionedShowcaseUseCaseTest : BehaviorSpec() {
   override fun isolationMode(): IsolationMode = IsolationMode.InstancePerLeaf
 
-  private var repositoryData = emptyList<Showcase>()
+  private var repositoryData = Result.success(emptyList<Showcase>())
   private val repository: ShowcaseRepository = mockk()
   private val useCase: GetPartitionedShowcasesUseCase = GetPartitionedShowcasesUseCase(
     showcaseRepository = repository
@@ -25,21 +25,17 @@ class GetPartitionedShowcaseUseCaseTest : BehaviorSpec() {
     super.beforeSpec(spec)
 
     coEvery { repository.loadShowcases() } answers { repositoryData }
-    coEvery { repository.update(any()) } answers {
-      val showcases = firstArg<List<Showcase>>()
-      repositoryData = showcases
-    }
   }
 
   init {
-    Given("repository에 데이터가 없는 경우") {
-      repositoryData = emptyList()
+    Given("repository에 데이터가 빈 데이터인 경우") {
+      repositoryData = Result.success(emptyList())
 
       When("빈 idToPartitionCount 가 주어졌을 때") {
         val idToPartitionCount: Map<String, Int> = emptyMap()
 
         Then("빈 값을 받는다") {
-          useCase(idToPartitionCount) shouldBe emptyList()
+          useCase(idToPartitionCount) shouldBe repositoryData
         }
       }
 
@@ -47,7 +43,27 @@ class GetPartitionedShowcaseUseCaseTest : BehaviorSpec() {
         val idToPartitionCount = mapOf("INVALID ID" to 3)
 
         Then("빈 값을 받는다") {
-          useCase(idToPartitionCount) shouldBe emptyList()
+          useCase(idToPartitionCount) shouldBe repositoryData
+        }
+      }
+    }
+
+    Given("repository에 데이터가 실패 데이터인 경우") {
+      repositoryData = Result.failure(Exception())
+
+      When("빈 idToPartitionCount 가 주어졌을 때") {
+        val idToPartitionCount: Map<String, Int> = emptyMap()
+
+        Then("실패 데이터를 받는다") {
+          useCase(idToPartitionCount) shouldBe repositoryData
+        }
+      }
+
+      When("repository에 존재하는 id가 없는 idToPartitionCount 가 주어졌을 때") {
+        val idToPartitionCount = mapOf("INVALID ID" to 3)
+
+        Then("실패 데이터를 받는다") {
+          useCase(idToPartitionCount) shouldBe repositoryData
         }
       }
     }
@@ -56,16 +72,16 @@ class GetPartitionedShowcaseUseCaseTest : BehaviorSpec() {
       val partitionableContents: Contents.StyleContents =
         Fixtures.partitionableShowcase.contents as Contents.StyleContents
 
-      repositoryData = listOf(Fixtures.unpartitionableShowcase, Fixtures.partitionableShowcase)
+      repositoryData = Result.success(listOf(Fixtures.unpartitionableShowcase, Fixtures.partitionableShowcase))
 
       When("빈 idToPartitionCount 가 주어졌을 때") {
         val idToPartitionCount: Map<String, Int> = emptyMap()
 
         Then("Partitionable하지 않은 데이터는 변경되지 않는다") {
-          useCase(idToPartitionCount)[0].contents shouldBe Fixtures.unpartitionableShowcase.contents
+          useCase(idToPartitionCount).get(0).contents shouldBe Fixtures.unpartitionableShowcase.contents
         }
         Then("Partitionable한 데이터는 기본 갯수로 partition된다") {
-          useCase(idToPartitionCount)[1].contents shouldBe partitionableContents.copy(
+          useCase(idToPartitionCount).get(1).contents shouldBe partitionableContents.copy(
             items = partitionableContents.items.take(partitionableContents.partitionInfo.defaultCount)
           )
         }
@@ -75,10 +91,10 @@ class GetPartitionedShowcaseUseCaseTest : BehaviorSpec() {
         val idToPartitionCount = mapOf("INVALID ID" to 3)
 
         Then("Partitionable하지 않은 데이터는 변경되지 않는다") {
-          useCase(idToPartitionCount)[0].contents shouldBe Fixtures.unpartitionableShowcase.contents
+          useCase(idToPartitionCount).get(0).contents shouldBe Fixtures.unpartitionableShowcase.contents
         }
         Then("Partitionable한 데이터는 기본 갯수로 partition된다") {
-          useCase(idToPartitionCount)[1].contents shouldBe Fixtures.defaultPartitionedShowcase.contents
+          useCase(idToPartitionCount).get(1).contents shouldBe Fixtures.defaultPartitionedShowcase.contents
         }
       }
 
@@ -87,16 +103,16 @@ class GetPartitionedShowcaseUseCaseTest : BehaviorSpec() {
           val idToPartitionCount = mapOf(Fixtures.partitionableShowcase.id to 100)
 
           Then("Partitionable하지 않은 데이터는 변경되지 않는다") {
-            useCase(idToPartitionCount)[0].contents shouldBe Fixtures.unpartitionableShowcase.contents
+            useCase(idToPartitionCount).get(0).contents shouldBe Fixtures.unpartitionableShowcase.contents
           }
           Then("Partitionable한 데이터는 전체 데이터가 반환된다") {
-            useCase(idToPartitionCount)[1].contents shouldBe Fixtures.partitionableShowcase.contents
+            useCase(idToPartitionCount).get(1).contents shouldBe Fixtures.partitionableShowcase.contents
           }
           Then("Partitionable하지 않은 데이터는 footer가 있다") {
-            useCase(idToPartitionCount)[0].footer shouldNotBe null
+            useCase(idToPartitionCount).get(0).footer shouldNotBe null
           }
           Then("Partitionable한 데이터는 footer가 없다") {
-            useCase(idToPartitionCount)[1].footer shouldBe null
+            useCase(idToPartitionCount).get(1).footer shouldBe null
           }
         }
 
@@ -104,16 +120,16 @@ class GetPartitionedShowcaseUseCaseTest : BehaviorSpec() {
           val idToPartitionCount = mapOf(Fixtures.partitionableShowcase.id to Fixtures.partitionableShowcase.contents.items.size)
 
           Then("Partitionable하지 않은 데이터는 변경되지 않는다") {
-            useCase(idToPartitionCount)[0].contents shouldBe Fixtures.unpartitionableShowcase.contents
+            useCase(idToPartitionCount).get(0).contents shouldBe Fixtures.unpartitionableShowcase.contents
           }
           Then("Partitionable한 데이터는 변경되지 않는다") {
-            useCase(idToPartitionCount)[1].contents shouldBe Fixtures.partitionableShowcase.contents
+            useCase(idToPartitionCount).get(1).contents shouldBe Fixtures.partitionableShowcase.contents
           }
           Then("Partitionable하지 않은 데이터는 footer가 있다") {
-            useCase(idToPartitionCount)[0].footer shouldNotBe null
+            useCase(idToPartitionCount).get(0).footer shouldNotBe null
           }
           Then("Partitionable한 데이터는 footer가 없다") {
-            useCase(idToPartitionCount)[1].footer shouldBe null
+            useCase(idToPartitionCount).get(1).footer shouldBe null
           }
         }
 
@@ -122,32 +138,32 @@ class GetPartitionedShowcaseUseCaseTest : BehaviorSpec() {
           val idToPartitionCount = mapOf(Fixtures.partitionableShowcase.id to count)
 
           Then("Partitionable하지 않은 데이터는 변경되지 않는다") {
-            useCase(idToPartitionCount)[0].contents shouldBe Fixtures.unpartitionableShowcase.contents
+            useCase(idToPartitionCount).get(0).contents shouldBe Fixtures.unpartitionableShowcase.contents
           }
           Then("Partitionable한 데이터는 PartitionCount만큼 partition된다") {
-            useCase(idToPartitionCount)[1].contents.items shouldBe partitionableContents.items.take(count)
+            useCase(idToPartitionCount).get(1).contents.items shouldBe partitionableContents.items.take(count)
           }
           Then("Partitionable하지 않은 데이터는 footer가 있다") {
-            useCase(idToPartitionCount)[0].footer shouldNotBe null
+            useCase(idToPartitionCount).get(0).footer shouldNotBe null
           }
           Then("Partitionable한 데이터는 footer가 있다") {
-            useCase(idToPartitionCount)[1].footer shouldNotBe null
+            useCase(idToPartitionCount).get(1).footer shouldNotBe null
           }
         }
       }
     }
 
     Given("repository에 partitionable 데이터 아이템이 기본 parition 갯수보다 부족한 경우") {
-      repositoryData = listOf(Fixtures.emptyPartitionableShowcase)
+      repositoryData = Result.success(listOf(Fixtures.emptyPartitionableShowcase))
 
       When("빈 idToPartitionCount가 주어졌을 때") {
         val idToPartitionCount: Map<String, Int> = emptyMap()
 
         Then("빈 값이 반환된다") {
-          useCase(idToPartitionCount)[0].contents shouldBe Fixtures.emptyPartitionableShowcase.contents
+          useCase(idToPartitionCount).get(0).contents shouldBe Fixtures.emptyPartitionableShowcase.contents
         }
         Then("footer가 없다") {
-          useCase(idToPartitionCount)[0].footer shouldBe null
+          useCase(idToPartitionCount).get(0).footer shouldBe null
         }
       }
 
@@ -155,12 +171,16 @@ class GetPartitionedShowcaseUseCaseTest : BehaviorSpec() {
         val idToPartitionCount = mapOf(Fixtures.partitionableShowcase.id to 5)
 
         Then("빈 값이 반환된다") {
-          useCase(idToPartitionCount)[0].contents shouldBe Fixtures.emptyPartitionableShowcase.contents
+          useCase(idToPartitionCount).get(0).contents shouldBe Fixtures.emptyPartitionableShowcase.contents
         }
         Then("footer가 없다") {
-          useCase(idToPartitionCount)[0].footer shouldBe null
+          useCase(idToPartitionCount).get(0).footer shouldBe null
         }
       }
     }
+  }
+
+  private fun Result<List<Showcase>>.get(index: Int): Showcase {
+    return this.getOrDefault(emptyList()).get(index)
   }
 }
