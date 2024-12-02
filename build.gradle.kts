c3be686f045eac11952a0868a91d23a84f9e5e50
Yebon Kim <v954242@gmail.com>
2024-12-02 14:18:31 +0900

    chore : check compose stability

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,3 +10,18 @@ plugins {
   alias(libs.plugins.hilt) apply false
   alias(libs.plugins.kotlin.parcelize) apply false
 }
+
+subprojects {
+  tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().all {
+    kotlinOptions.freeCompilerArgs += listOf(
+      "-P",
+      "plugin:androidx.compose.compiler.plugins.kotlin:reportsDestination=" +
+        project.buildDir.absolutePath + "/compose_metrics"
+    )
+    kotlinOptions.freeCompilerArgs += listOf(
+      "-P",
+      "plugin:androidx.compose.compiler.plugins.kotlin:metricsDestination=" +
+        project.buildDir.absolutePath + "/compose_metrics"
+    )
+  }
+}
